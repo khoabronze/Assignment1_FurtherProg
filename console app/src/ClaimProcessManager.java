35d5955c4a7740c5f3ee477f2b50d5525bca263d
khoa1002 <s3986281@rmit.edu.vn>
2024-03-20 03:54:32 +0700

    a

---
+++ console app/src/ClaimProcessManager.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public abstract interface ClaimProcessManager {
+
+
+        // Add a new claim to the system
+        void add(Claim claim);
+
+        // Update an existing claim in the system
+        void update(Claim claim);
+
+        // Delete a claim from the system
+        void delete(String claimId);
+
+        // Retrieve a specific claim from the system based on its ID
+        Claim getOne(String claimId);
+
+        // Retrieve all claims from the system
+        ArrayList<Claim> getAll();
+    }
+
--- console app/src/Customer.java
+++ console app/src/Customer.java
@@ -1,8 +1,23 @@
 import java.util.ArrayList;
 
-public class Customer {
+public class Customer  {
     private String id;
     private String Fullname;
     private InsuranceCard insuranceCard;
     private ArrayList<Claim> claims;
+
+    public Customer(){
+        this.id = "default";
+        Fullname = "default";
+        this.insuranceCard = null;
+        this.claims = null;
+    }
+
+
+    public Customer(String id, String fullname, InsuranceCard insuranceCard, ArrayList<Claim> claims) {
+        this.id = id;
+        Fullname = fullname;
+        this.insuranceCard = insuranceCard;
+        this.claims = claims;
+    }
 }
